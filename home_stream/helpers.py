ac226b4322bdad3374c08a43d99ac37194fa32c8
Max Mehl <mail@mehl.mx>
2026-07-28 16:21:25 +0200

    chore: fix failing ruff checks

--- home_stream/helpers.py
+++ home_stream/helpers.py
@@ -174,7 +174,7 @@ def get_version_info() -> str:
     # Get short git commit hash if available
     try:
         commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()  # noqa: S607
-    except Exception:  # noqa: BLE001
+    except Exception:
         current_app.logger.debug("Failed to get git commit hash.", exc_info=True)
         commit = "unknown commit"
 
